cdc15a1d8c76a5ab3ebc24e6e83190a493d2cb8c
Aks-l <wiersdalenaksel@gmail.com>
2025-02-10 16:48:34 +0100

    file structure

---
+++ oblig1/oblig1/RPS.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.26;
+
+contract RPC{
+
+    event JoinGame();
+    event revealVote();
+    event withdrawGame();
+
+    string[3] votes = ["R", "P", "S"];
+
+    function play(string memory vote) public {
+        
+    }
+
+    function reveal() public {
+
+    }
+
+    function withdraw() public {
+
+    }
+
+}
