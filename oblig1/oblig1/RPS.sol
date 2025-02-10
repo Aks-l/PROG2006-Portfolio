73ca58110ca6bea52c9d646d6afee89a1358f261
Aks-l <wiersdalenaksel@gmail.com>
2025-02-10 19:01:36 +0100

    game-struct

--- oblig1/oblig1/RPS.sol
+++ oblig1/oblig1/RPS.sol
@@ -3,22 +3,35 @@ pragma solidity ^0.8.26;
 
 contract RPC{
 
-    event JoinGame();
-    event revealVote();
-    event withdrawGame();
+    struct Game{
+        string id;
+        bytes32 p1;
+        bytes32 p2;
+        string v1;
+        string v2;
+        bytes32 winner;
+        int stake;
+    }
+
+    event joinGame(address sender, bytes32 vote);
+    event revealVote(address sender, string vote);
+    event withdrawGame(address sender, int amount);
+
+    int minStake = 2;
 
     string[3] votes = ["R", "P", "S"];
 
     function play(string memory vote) public {
         
     }
+    
+    function withdraw() public {
+
+    }
 
     function reveal() public {
 
     }
 
-    function withdraw() public {
-
-    }
 
 }
