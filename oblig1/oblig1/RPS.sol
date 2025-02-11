dacb399fe56d13551b65558a0cebee02276e1ae5
Aks-l <wiersdalenaksel@gmail.com>
2025-02-11 13:01:34 +0100

    play function

--- oblig1/oblig1/RPS.sol
+++ oblig1/oblig1/RPS.sol
@@ -5,13 +5,19 @@ contract RPC{
 
     struct Game{
         string id;
-        bytes32 p1;
-        bytes32 p2;
-        string v1;
-        string v2;
-        bytes32 winner;
+        address p1;
+        address p2;
+        bytes32 v1;
+        bytes32 v2;
+        address winner;
         int stake;
     }
+
+    // maps player to game
+    mapping(address => Game) public activeGames;
+
+    // map game id to owner (game.p1)
+    mapping(string => address) public gameOwners;
 
     event joinGame(address sender, bytes32 vote);
     event revealVote(address sender, string vote);
@@ -21,8 +27,41 @@ contract RPC{
 
     string[3] votes = ["R", "P", "S"];
 
-    function play(string memory vote) public {
+    function play(bytes32 _vote, int _stake, string memory _id) public payable {
+        // if any game has the same stake, join that game
+        if (_stake < minStake)
+            return;
+
+        // if game does not exist
+        if (activeGames[msg.sender].p1 != address(0))
+            return;
         
+        // if game has no owner, create new game
+        if (gameOwners[_id] == address(0)){
+
+            activeGames[msg.sender] = Game({
+                id: _id,
+                p1: msg.sender,
+                p2: address(0),
+                v1: _vote,
+                v2: bytes32(0),
+                winner: address(0),
+                stake: _stake
+            });
+
+            gameOwners[_id] == msg.sender;
+        }
+        // otherwise join game
+        // find game through p1 map
+        else {
+            Game storage toJoin = activeGames[gameOwners[_id]];
+            if (toJoin.stake != _stake)
+                return;
+
+            toJoin.p2 = msg.sender;
+            toJoin.v2 = _vote;
+
+        }
     }
     
     function withdraw() public {
@@ -30,6 +69,7 @@ contract RPC{
     }
 
     function reveal() public {
+    
 
     }
 
