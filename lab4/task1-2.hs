70df7e5d21cbaee55567878869ebfba38d9fde74
Aks-l <wiersdalenaksel@gmail.com>
2025-02-26 13:51:16 +0100

    t push

---
+++ lab4/task1-2.hs
@@ -0,0 +1,20 @@
+import Text.Printf
+
+mreverse :: [a] -> [a]
+mreverse [] = []
+mreverse (x:xs) = mreverse xs ++ [x]
+
+
+mulTable :: Int -> IO ()
+mulTable n = do
+    let table = [ [i*j | i <- [1..n]] | j <- [1..n] ]
+    mapM_ (putStrLn . unwords . map (printf "%3d")) table
+
+
+{-
+maxage=0
+out=1
+for age in ages:
+    if age>maxage: out=1
+    elif age==maxage: out+=1
+-}
